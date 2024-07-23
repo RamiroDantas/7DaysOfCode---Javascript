03bc2e4dc86b1d62874ce51aa9e198a8445f020a
Ramiro Dantas <ramiro.dantas@meucorban.app>
2024-07-23 16:30:27 -0300

    Dia 2 do desafio de javascript concluido

---
+++ Dia2/script.js
@@ -0,0 +1,33 @@
+function handleSubmitData(){
+  event.preventDefault();
+  var nameUser = document.getElementById('name').value;
+  var ageUser = document.getElementById('age').value;
+  var languageUser = document.getElementById('language').value;
+
+
+  if(!nameUser) {
+    alert("Digite seu nome para continuar");
+    return;
+  }
+  
+  if(!ageUser) {
+    alert("Digite sua idade para continuar");
+    return;
+  }
+
+  if(!languageUser) {
+    alert("Digite uma linguagem para continuar");
+    return;
+  }
+
+  alert("Olá " + nameUser + ", você tem " + ageUser + " anos e já está aprendendo " + languageUser + "!");
+
+
+  var feedback = prompt("Você gosta de estudar " + languageUser + "?\nResponda com número 1 para SIM ou 2 para NÃO");
+
+  if(feedback == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+  } else if(feedback == 2) {
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+  }
+}
